ef05ac8a41a591f588a0509726a3022dd9758ca7
imelda-1 <im.ishimwe@gmail.com>
2023-05-24 15:47:04 -0700

    realloc

--- realloc.c
+++ realloc.c
@@ -20,7 +20,7 @@ char *_memset(char *s, char b, unsigned int n)
  * ffree - frees string of strings
  * @pp: String of strings
  */
-void ffree(char **pp)
+void free(char **pp)
 {
 	char **a = pp;
 
